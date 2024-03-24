e386fc52c0b0d29a58bf786c26010c3f3e5a4fb5
Quvonchbek0203 <quvonchbeknormurodov62@gmail.com>
2024-03-25 00:24:41 +0500

    header
    header

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,16 +10,16 @@ import logo from "../assets/qmii-logo.png"
 const Navbar = () => {
   const [toggle,setToggle] = useState(false)
   return (
-    <div className='containerr flex justify-between items-center bg-[white] h-[100px] w-full'>
+    <div className='containerr z-[10] flex fixed justify-between items-center bg-[white] h-[100px] w-full p-10'>
       {/* Logo  */}
       <div className='w-[80px] md:w-[120px] cursor-pointer'>
         <img src={logo} alt="logo" className='w-full' />    
       </div>
 
       {/* nav-items  */}
-      <div className={`text-[#0a2050] md:block md:static md:w-full absolute top-[80px] w-[50%] ${toggle ? 'left-0 backdrop-blur' : 'left-[-100%]'} transition-none`}>
+      <div className={`text-[#0a2050] md:block lg: md:static md:w-full absolute top-[80px] w-[50%] ${toggle ? 'left-0 backdrop-blur ' : 'left-[-100%]'} transition-none`}>
         <nav>
-          <ul className='flex md:flex-row md:justify-end flex-col gap-10 md:text-[18px] lg:text-[22px] font-bolt'>
+          <ul className='flex md:flex-row md:justify-end flex-col gap-10 md:text-[18px] lg:text-[22px] font-bold'>
             <li className='hover:text-[#e0e05f]'><a href="#">Home</a></li>
             <li className='hover:text-[#e0e05f]'><a href="#">About</a></li>
             <li className='hover:text-[#e0e05f]'><a href="#">Blog</a></li>
@@ -30,7 +30,7 @@ const Navbar = () => {
       </div>
 
       {/* bar icon  */}
-      <div className='md:hidden block- text-[25px]' onClick={()=>setToggle(!toggle)}>
+      <div className='md:hidden block text-[25px]' onClick={()=>setToggle(!toggle)}>
         {toggle ? <span><MdOutlineCancel /></span> : <span><FaBars /></span>}
       </div>
 
